511ab8d9def9d90e5a58e871645bfae896cfa1d2
Paul Ivey <paivey@microsoft.com>
2022-01-26 11:04:07 +0000

    Removed Terraform

--- Terraform/Methanol/Shared/data-sources.tf
+++
@@ -1,8 +0,0 @@
-data "azurerm_resource_group" "rg" {
-  name = "RG-${upper(var.terraform_rg_suffix)}-${upper(var.region_short_name)}-${upper(var.project_name)}-${var.resource_suffix}"
-}
-
-data "azurerm_key_vault" "persistent-kv" {
-  name                = local.keyvault_name
-  resource_group_name = "RG-${upper(var.terraform_rg_suffix)}-${upper(var.region_short_name)}-${upper(var.methanol-project-name)}-${var.resource_suffix}"
-}
